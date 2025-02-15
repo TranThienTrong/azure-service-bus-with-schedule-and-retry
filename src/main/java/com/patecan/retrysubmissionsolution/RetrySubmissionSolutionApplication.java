24ae6981dfbfc7499184ec56451460999f7dc6e5
Trần Thiên Trọng <tranthientrong.it@gmail.com>
2025-02-15 19:07:52 +0700

    First init all

---
+++ src/main/java/com/patecan/retrysubmissionsolution/RetrySubmissionSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.patecan.retrysubmissionsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrySubmissionSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrySubmissionSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patecan/retrysubmissionsolution/RetrySubmissionSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patecan.retrysubmissionsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrySubmissionSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
